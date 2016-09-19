a43e5cb24c4acacdc705ebb379078935da4ab2a7
T.DW <dewang.tian.2014@sis.smu.edu.sg>
2016-09-19 14:27:30 +0800

    timer pic resolution form pic text color (white change to black)

--- app/src/main/java/com/wegot/fuyan/fyp/CompletedRequest.java
+++ app/src/main/java/com/wegot/fuyan/fyp/CompletedRequest.java
@@ -2,6 +2,8 @@ package com.wegot.fuyan.fyp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class CompletedRequest extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class CompletedRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_completed_request);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        toolbar.setTitle("Request Completed");
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
