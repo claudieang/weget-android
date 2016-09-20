445e5cae480a288c9b24837c619e34d2ca908ad3
T.DW <dewang.tian.2014@sis.smu.edu.sg>
2016-09-20 16:58:56 +0800

    update bank details, 1 page new layout

---
+++ app/src/main/java/com/wegot/fuyan/fyp/update_bank_details.java
@@ -0,0 +1,13 @@
+package com.wegot.fuyan.fyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class update_bank_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_bank_details);
+    }
+}
